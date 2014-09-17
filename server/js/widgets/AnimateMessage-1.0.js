fb697a9163bf64a910ace45db888c69bc4aa7cbc
Alex S. Marinenko <alex.marinenko@gmail.com>
2014-09-17 23:36:10 +0400

    issue-1025

--- server/js/widgets/AnimateMessage-1.0.js
+++ server/js/widgets/AnimateMessage-1.0.js
@@ -12,30 +12,6 @@ var AnimateMessage = function() {
         $.colorbox.close();
     };
     self.ListMessage = function() {
-        jQuery('.activeHoverElement .actAreaShow').click(function() {
-            $(this).closest(".horizontalSection").next().show();
-            $(this).closest(".horizontalSection").hide();
-            return false;
-        });
-        jQuery('.floatFieldsFormSetion .actAreaHide').click(function() {
-            $(this).closest(".floatFieldsFormSetion").prev().show();
-            $(this).closest(".floatFieldsFormSetion").hide();
-            return false;
-        });
-        jQuery('.expandCollapse.expand').click(function() {
-            $(this).closest(".htabsContent").find(".activeHoverElement").next().show();
-            $(this).closest(".htabsContent").find(".activeHoverElement").hide();
-            $(this).hide();
-            $(".expandCollapse.collapse").show();
-            return false;
-        });
-        jQuery('.expandCollapse.collapse').click(function() {
-            $(this).closest(".htabsContent").find(".activeHoverElement").next().hide();
-            $(this).closest(".htabsContent").find(".activeHoverElement").show();
-            $(this).hide();
-            $(".expandCollapse.expand").show();
-            return false;
-        });
     };
 };
 
