70331daa0e0aaebd668b83afd4e5accdd7f50694
Alex S. Marinenko <alex.marinenko@gmail.com>
2014-09-21 22:46:47 +0400

    issue-1057

--- server/js/widgets/AnimateGoods-1.0.js
+++ server/js/widgets/AnimateGoods-1.0.js
@@ -1,4 +1,19 @@
 var AnimateGoods = function(){
     PKP.UI.init();
+    $('.b-catalog-item__photos').fotorama({
+        width: 313,
+        allowfullscreen: true,
+        loop: true,
+        autoplay: false,
+        stopautoplayontouch: true,
+        nav: 'thumbs',
+        thumbwidth: 65,
+        thumbheigth: 60,
+        thumbmargin: 5,
+        thumbborderwidth: 4,
+        arrows: false,
+        shadows: true,
+        transition: 'slide',
+    });
 }
 
