fd175b897339f9aaec30c0a8efe2132b86947248
Alex S. Marinenko <alex.marinenko@gmail.com>
2014-09-29 01:42:37 +0400

    issue-1057

--- server/js/widgets/AnimateRegistration-1.0.js
+++ server/js/widgets/AnimateRegistration-1.0.js
@@ -1,5 +1,8 @@
 var AnimateRegistration = function () {
     if (Routing.route == 'registration') {
+        $('#phone').mask("?9 999 999 99 99 99");
+        $(":input:not(:checkbox):not(:button):not([type=hidden]):not([type=search]):not(.no-label)").floatlabel();
+    
         var step = Routing.params.step;
         var progress = 0;
         var current = step / 4;
